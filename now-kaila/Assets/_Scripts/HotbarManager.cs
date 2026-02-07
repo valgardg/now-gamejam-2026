2b52ed3efbe72babfa5a09d0049a866f9704f776
soloddsdottir-dotcom <soloddsdottir@gmail.com>
2026-02-07 20:01:02 +0100

    added an audio slider
    Super quick to make, if we decide to add music i thought it would be cool if we could use this audio slider to control all the game audio

--- now-kaila/Assets/_Scripts/HotbarManager.cs
+++ now-kaila/Assets/_Scripts/HotbarManager.cs
@@ -5,12 +5,14 @@ public class HotbarManager : MonoBehaviour
     [Header("UI information")]
     public GameObject chatterBoxPanel;
     public GameObject socialMediaPanel;
+    public GameObject audioSliderVisual;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         chatterBoxPanel.SetActive(false);
         socialMediaPanel.SetActive(true);
+        audioSliderVisual.SetActive(false);
     }
 
     public void ToggleChatterBoxPanel()
@@ -22,7 +24,10 @@ public class HotbarManager : MonoBehaviour
     {
         socialMediaPanel.SetActive(!socialMediaPanel.activeSelf);
     }
-
+    public void ToggleAudioSliderVisual()
+    {
+        audioSliderVisual.SetActive(!audioSliderVisual.activeSelf);
+    }
     // Update is called once per frame
     void Update()
     {
